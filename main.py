05cc6479a0bf322d629f774671ac0b3953de95f5
aletricca02 <aletricca18@gmail.com>
2024-11-17 19:23:21 +0100

    Codice funzionante no entropy

--- main.py
+++ main.py
@@ -1,142 +1,145 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import time
-from aima.search import iterative_deepening_search
+from aima.search import *
 from nao_problem import NaoProblem
-from utils import from_state_to_dict, play_song, do_moves
+from utils import *
+
+
+def precondition_standing(position):
+    if position == 'M_SitRelax':
+        return False
+    return True
+
+
+def postcondition_standing(position):
+    if position in ('M_Sit', 'M_SitRelax'):
+        return False
+    return True
 
 
 class NaoMove:
-    """Classe per rappresentare un movimento del robot."""
-    def __init__(self, duration, preconditions=None, postconditions=None):
+    """
+    This class defines the information related
+    to a particular move.
+    """
+    def __init__(self, duration=None, preconditions=None, postconditions=None):
         self.duration = duration
-        self.preconditions = preconditions if preconditions else {}
-        self.postconditions = postconditions if postconditions else {}
-
-def initialize_moves():
-    """Definisce i movimenti disponibili per il robot."""
-    return {
-        'StandUp': NaoMove(1.21, {'standing': False}, {'standing': True}),
-        'AirGuitar': NaoMove(5.4, {'standing': True}, {'standing': True}),
-        'ArmDance': NaoMove(13.03, {'standing': True}, {'standing': True}),
-        'ArmsOpening': NaoMove(8.15, {'standing': True}, {'standing': True}),
-        'BlowKisses': NaoMove(5.27, {'standing': True}, {'standing': True}),
-        'Bow': NaoMove(4.81, {'standing': True}, {'standing': True}),
-        'Clap': NaoMove(4.99, {'standing': True}, {'standing': True}),
-        'ComeOn': NaoMove(5.35, {'standing': True}, {'standing': True}),
-        'DanceMove': NaoMove(7.49, {'standing': True}, {'standing': True}),
-        'DiagonalLeft': NaoMove(4.17, {'standing': True}, {'standing': True}),
-        'DiagonalRight': NaoMove(6.46, {'standing': True}, {'standing': True}),
-        'DoubleMovementGloryJoy': NaoMove(0.07, {'standing': True}, {'standing': True}),
-        'Joy': NaoMove(4.50, {'standing': True}, {'standing': True}),
-        'M_Crouch': NaoMove(1.65, {'standing': True}, {'standing': False}),
-        'M_Hello': NaoMove(5.47, {'standing': True}, {'standing': True}),
-        'M_SitRelax': NaoMove(15.46, {'standing': True}, {'sitting': True}),
-        'M_Sit': NaoMove(11.37, {'standing': True}, {'sitting': True}),
-        'M_Stand': NaoMove(3.45, {'sitting': True}, {'standing': True}),
-        'M_StandInit': NaoMove(1.96, {'standing': True}, {'standing': True}),
-        'M_StandZero': NaoMove(2.77, {'standing': True}, {'standing': True}),
-        'M_WipeForehead': NaoMove(5.44, {'standing': True}, {'standing': True}),
-        'MoveBackward': NaoMove(4.79, {'standing': True}, {'standing': True}),
-        'MoveForward': NaoMove(3.75, {'standing': True}, {'standing': True}),
-        'PulpFiction': NaoMove(6.83, {'standing': True}, {'standing': True}),
-        'Rhythm': NaoMove(4.97, {'standing': True}, {'standing': True}),
-        'RightArm': NaoMove(14.26, {'standing': True}, {'standing': True}),
-        'RotationFootLLeg': NaoMove(7.5, {'standing': True}, {'standing': True}),
-        'RotationFootRLeg': NaoMove(7.64, {'standing': True}, {'standing': True}),
-        'SprinklerL': NaoMove(5.08, {'standing': True}, {'standing': True}),
-        'SprinklerR': NaoMove(5.19, {'standing': True}, {'standing': True}),
-        'StayingAlive': NaoMove(11.93, {'standing': True}, {'standing': True}),
-        'TheRobot': NaoMove(7.56, {'standing': True}, {'standing': True}),
-        'Union_arms': NaoMove(10.31, {'standing': True}, {'standing': True}),
-        'Wave': NaoMove(5.99, {'standing': True}, {'standing': True}),
-    }
-
-
-def initialize_positions():
-    """Definisce le posizioni obbligatorie e iniziali/finali."""
-    initial_pos = ('M_StandInit', NaoMove(1.60))
-    mandatory_positions = [
-        ('M_WipeForehead', NaoMove(4.48)),
-        ('M_Stand', NaoMove(2.32)),
-        ('M_Hello', NaoMove(4.34)),
-        ('M_Sit', NaoMove(9.84)),
-        ('M_SitRelax', NaoMove(3.92)),
-        ('M_StandZero', NaoMove(1.4)),
-    ]
-    final_goal_pos = ('M_Crouch', NaoMove(1.32))
-    return [initial_pos, *mandatory_positions, final_goal_pos]
+        self.preconditions = preconditions if preconditions is not None else {}
+        self.postconditions = postconditions if preconditions is not None else {}
 
 
 def main(robot_ip, port):
-    moves = initialize_moves()
-    positions = initialize_positions()
-    total_steps = len(positions) - 1
+    # TODO: win the challenge :-)
+    # The following ones are the moves made available to the robot:
+    moves = {'StandUp':       NaoMove(8.35,  {'standing': False}, {'standing': True}),
+             'AirGuitar':     NaoMove(4.10,   {'standing': True},  {'standing': True}),
+             'ArmDance':      NaoMove(10.42, {'standing': True},  {'standing': True}),
+             'BlowKisses':    NaoMove(4.58,  {'standing': True},  {'standing': True}),
+             'Bow':           NaoMove(3.86,   {'standing': True},  {'standing': True}),
+             'DiagonalRight': NaoMove(2.56,  {'standing': True},  {'standing': True}),
+             'DanceMove':     NaoMove(6.13,  {'standing': True},  {'standing': True}),
+             'SprinklerL':    NaoMove(4.14,   {'standing': True},  {'standing': True}),
+             'SprinklerR':    NaoMove(4.36,  {'standing': True},  {'standing': True}),
+             'RightArm':      NaoMove(9.19,  None, None),
+             'TheRobot':      NaoMove(6.10,   {'standing': True},  {'standing': True}),
+             'ComeOn':        NaoMove(3.62,   {'standing': True},  {'standing': True}),
+             'StayingAlive':  NaoMove(5.90,   {'standing': True},  {'standing': True}),
+             'Rhythm':        NaoMove(2.95,  {'standing': True},  {'standing': True}),
+             'PulpFiction':   NaoMove(5.8,   {'standing': True},  {'standing': True}),
+             'Wave':          NaoMove(3.72,  None, None),
+             'Glory':         NaoMove(3.28,  None, None),
+             'Clap':          NaoMove(4.10,  None, None),
+             'Joy':           NaoMove(4.50,  None, None)}
 
-    # Calcola il tempo medio perso nelle posizioni obbligatorie
-    total_time_lost = sum(pos[1].duration for pos in positions)
-    mean_time_lost_per_step = total_time_lost / total_steps
+    # The following is the order we chose for the mandatory positions:
+    initial_pos = ('M_StandInit',       NaoMove(1.60))
+    mandatory_pos = [('M_WipeForehead', NaoMove(4.48)),
+                     ('M_Stand',        NaoMove(2.32)),
+                     ('M_Hello',        NaoMove(4.34)),
+                     ('M_Sit',          NaoMove(9.84)),
+                     ('M_SitRelax',     NaoMove(3.92)),
+                     ('M_StandZero',    NaoMove(1.4))]
+    final_goal_pos = ('M_Crouch',       NaoMove(1.32))
+    pos_list = [initial_pos, *mandatory_pos, final_goal_pos]
+    number_of_steps = len(pos_list) - 1
+    # Lista per tracciare le mosse già eseguite
+    executed_moves = []
+    # Here we compute the total time lost during the
+    # entire choreography for the execution of mandatory moves
+    total_time = 0.0
+    for pos in pos_list:
+        total_time += pos[1].duration
+    # We consider 'total_time' as being
+    # evenly spread over each planning step:
+    mean_time_lost_because_of_mandatory_positions = total_time / number_of_steps
 
-    solution = ()
-    print("PIANIFICAZIONE DELLA COREOGRAFIA:")
+    # Planning phase of the algorithm
+    solution = tuple()
+    print("PLANNED CHOREOGRAPHY:")
     start_planning = time.time()
+    last_move = None
+    for index in range(1, len(pos_list)):
+        # The planning is done in several distinct steps: each
+        # one of them consists in solving a tree search in the space
+        # of possible choreographies between a mandatory position
+        # and the next one.
+        starting_pos = pos_list[index - 1]
+        ending_pos = pos_list[index]
+        if ending_pos[0] in executed_moves:
+            print(f"Skipping move {ending_pos[0]} as it was already executed.")
+            print(f"Moves executed so far: {executed_moves}")
+            continue  # Salta la mossa che è già stata eseguita
 
-    for step in range(1, len(positions)):
-        start_pos = positions[step - 1]
-        end_pos = positions[step]
-        remaining_time = 180.0 / total_steps - mean_time_lost_per_step
+        choreography = (starting_pos[0],)  # Initial choreography
+        initial_standing = postcondition_standing(starting_pos[0])
+        goal_standing = precondition_standing(ending_pos[0])
+        remaining_time = 180.0/number_of_steps - mean_time_lost_because_of_mandatory_positions
+        cur_state = (('choreography', choreography),
+                     ('standing', initial_standing),
+                     ('remaining_time', remaining_time),
+                     ('moves_done', 0))
+        cur_goal_state = (('standing', goal_standing),
+                          ('remaining_time', 0),  # About this amount of time left
+                          ('moves_done', 3))  
 
-        # Stato iniziale e goal per il problema
-        initial_state = {
-            'choreography': [start_pos[0]],
-            'standing': end_pos[1].preconditions.get('standing', True),
-            'remaining_time': remaining_time,
-            'moves_done': 0,
-        }
-        goal_state = {
-            'standing': end_pos[1].preconditions.get('standing', True),
-            'remaining_time': 0,
-            'moves_done': 3,  # Fissato a 3 mosse tra le posizioni obbligatorie
-        }
+        cur_problem = NaoProblem(cur_state, cur_goal_state, moves, 1, solution)
+        cur_solution = iterative_deepening_search(cur_problem)
+        if cur_solution is None:
+            raise RuntimeError(f'Step {index} - no solution was found!')
 
-        # Risoluzione con iterativa approfondita
-        problem = NaoProblem(initial_state, goal_state, moves)
-        partial_solution = iterative_deepening_search(problem)
-        if partial_solution is None:
-            raise RuntimeError(f"Passo {step}: nessuna soluzione trovata!")
-
-        # Aggiunge la coreografia trovata
-        state_dict = from_state_to_dict(partial_solution.state)
-        current_choreography = state_dict['choreography']
-        print(f"Passo {step}: {', '.join(current_choreography)}")
-        solution += tuple(current_choreography)
+        cur_solution_dict = from_state_to_dict(cur_solution.state)
+        cur_choreography = cur_solution_dict['choreography']
+        print(f"Step {index}: \t" + ", ".join(cur_choreography))
+        solution += cur_choreography
+        # Aggiungi la mossa corrente alla lista delle mosse eseguite
+        executed_moves.append(ending_pos[0]) 
 
     end_planning = time.time()
-    solution += (positions[-1][0],)
-
-    print("\nSTATISTICHE:")
-    print(f"Tempo di pianificazione: {end_planning - start_planning:.2f} secondi")
+    solution += (final_goal_pos[0],)
+    state_dict = from_state_to_dict(cur_solution.state)
+    print("\nSTATISTICS:")
+    print(f"Time required by the planning phase: %.2f seconds." % (end_planning-start_planning))
+    print(f"Estimated choreography duration: {180.0 - state_dict['remaining_time']}")
     print("-------------------------------------------------------")
-
-    # Esecuzione della coreografia
-    print("\nESECUZIONE DELLA DANZA:")
+    
+    # Dance execution
+    print("\nDANCE EXECUTION:")
     play_song("Don't stop me now - Queen.mp3")
     start_dance = time.time()
     do_moves(solution, robot_ip, port)
     end_dance = time.time()
-    print(f"Lunghezza della coreografia: {end_dance - start_dance:.2f} secondi.")
+    print("Length of the entire choreography: %.2f seconds." % (end_dance-start_dance))
 
 
 if __name__ == "__main__":
-    import sys
 
     robot_ip = "127.0.0.1"
-    port = 9559
+    port = 9559  # Insert NAO port
     if len(sys.argv) > 2:
         port = int(sys.argv[2])
         robot_ip = sys.argv[1]
     elif len(sys.argv) == 2:
         robot_ip = sys.argv[1]
-
+    
     main(robot_ip, port)
--- nao_problem.py
+++ nao_problem.py
@@ -1,55 +1,65 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import random
 from aima.search import Problem
 from utils import from_state_to_dict
-
-
 class NaoProblem(Problem):
-    def __init__(self, initial, goal, moves):
+
+    def __init__(self, initial, goal, moves, threshold, previous_moves_done):
         super().__init__(initial, goal)
-        self.moves = moves
+        self.available_moves = moves
+        self.previous_moves_done = previous_moves_done
+        self.time_threshold = threshold
 
     def is_move_applicable(self, state, move_name, move):
         state_dict = from_state_to_dict(state)
 
-        # Controllo tempo rimanente
+        # Controlla se il tempo rimanente è sufficiente
         if state_dict['remaining_time'] < move.duration:
             return False
 
-        # Controllo standing
+        # Controlla se le precondizioni del movimento sono rispettate
         if 'standing' in move.preconditions:
             if state_dict['standing'] != move.preconditions['standing']:
                 return False
 
-        # Nessuna mossa ripetuta consecutivamente
-        if state_dict['choreography'][-1] == move_name:
+        # Controlla che il movimento non sia identico all'ultimo eseguito
+        last_move = state_dict['choreography'][-1]
+        if move_name == last_move:
             return False
 
         return True
 
     def actions(self, state):
-        state_dict = from_state_to_dict(state)
-        return [move_name for move_name, move in self.moves.items() if self.is_move_applicable(state_dict, move_name, move)]
+        usable_actions = []
+        for move_name, move in self.available_moves.items():
+            if self.is_move_applicable(state, move_name, move):
+                usable_actions.append(move_name)
+        return usable_actions
 
     def result(self, state, action):
-        move = self.moves[action]
+        nao_move = self.available_moves[action]
         state_dict = from_state_to_dict(state)
 
-        # Aggiorna lo stato
-        return {
-            'choreography': state_dict['choreography'] + [action],
-            'standing': move.postconditions.get('standing', state_dict['standing']),
-            'remaining_time': state_dict['remaining_time'] - move.duration,
-            'moves_done': state_dict['moves_done'] + 1,
-        }
+        # Aggiorna lo stato con il nuovo movimento
+        new_standing = nao_move.postconditions.get('standing', state_dict['standing'])
+
+        return (('choreography', (*state_dict['choreography'], action)),
+                ('standing', new_standing),
+                ('remaining_time', state_dict['remaining_time'] - nao_move.duration),
+                ('moves_done', state_dict['moves_done'] + 1))
 
     def goal_test(self, state):
         state_dict = from_state_to_dict(state)
         goal_dict = from_state_to_dict(self.goal)
-        return (
-            state_dict['remaining_time'] >= goal_dict['remaining_time'] and
-            state_dict['moves_done'] >= goal_dict['moves_done'] and
-            state_dict['standing'] == goal_dict['standing']
-        )
+
+        goal_remaining_time = goal_dict['remaining_time']
+        a = goal_remaining_time
+        b = goal_remaining_time + self.time_threshold
+
+        # Verifica i vincoli del goal
+        time_constraint = (a <= state_dict['remaining_time'] <= b)
+        moves_done_constraint = (state_dict['moves_done'] >= goal_dict['moves_done'])
+        standing_constraint = (state_dict['standing'] == goal_dict['standing'])
+
+        if goal_dict['standing'] is None:
+            return time_constraint and moves_done_constraint
+        else:
+            return time_constraint and moves_done_constraint and standing_constraint

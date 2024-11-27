900d592403efd5a3661b7d5d7ea016694c6ee8d2
aletricca02 <aletricca18@gmail.com>
2024-11-27 15:56:29 +0100

    Codice aggiornato e commentato meglio

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Move:
         return self.Moveduration
 
 def main(robot_ip, port):
-    # Lista delle mosse utilizzate
+    # Dizionario delle mosse disponibili con la relativa durata
     PossibleMoves = {
         'AirGuitar': Move(4.10),
         'DanceTwist': Move(6.49),
@@ -46,13 +46,12 @@ def main(robot_ip, port):
         'Union_arms': Move(10.31),
     }
     
-    
-    # Lista per tracciare le mosse già eseguite
+    # Lista per tracciare le mosse già eseguite durante la coreografia
     executed_moves = []
 
     # Posizioni iniziali, obbligatorie e finali
-    starting_position = ('M_StandInit', Move(2.1))
-    mandatory_position = [
+    initial_position = ('M_StandInit', Move(2.1))
+    mandatory_positions = [
         ('M_WipeForehead', Move(5.48)),
         ('M_Stand', Move(2.82)),
         ('M_Hello', Move(5.19)),
@@ -62,104 +61,90 @@ def main(robot_ip, port):
     ]
     final_position = ('M_Crouch', Move(1.32))
 
-    pos_list = [starting_position, *mandatory_position, final_position]
-    number_of_steps = len(pos_list) - 1
+    # Lista di tutte le posizioni della coreografia
+    choreography_positions = [initial_position, *mandatory_positions, final_position]
+    number_of_steps = len(choreography_positions) - 1
 
+    # Calcolo del tempo totale delle mosse obbligatorie
+    total_time = sum(pos[1].Moveduration for pos in choreography_positions)
 
-    # Calcolare il tempo totale per le mosse obbligatorie
-    total_time = sum(pos[1].Moveduration for pos in pos_list)
+    # Tempo medio per ogni fase della coreografia
+    average_mandatory_time = total_time / number_of_steps
 
-    # Tempo totale per eseguire la mossa iniziale, le mosse mandatory e la mossa finale
-    mandatory_time = total_time / number_of_steps
-
-    # Fase di pianificazione
+    # Fase di pianificazione della coreografia
     print("PLANNED CHOREOGRAPHY:")
-    solution = tuple()
-    start_planning = time.time()
+    choreography_solution = tuple()  # Soluzione finale della coreografia
+    planning_start_time = time.time()  # Tempo di inizio pianificazione
     last_move = None
 
-    for index in range(1, len(pos_list)):
-        # Pianificazione tra posizione di partenza e posizione finale
-        starting_pos = pos_list[index - 1]
-        ending_pos = pos_list[index]
+    for index in range(1, len(choreography_positions)):
+        # Pianificazione tra posizione iniziale e posizione successiva
+        starting_position = choreography_positions[index - 1]
+        ending_position = choreography_positions[index]
 
-
-        if ending_pos[0] in executed_moves:
-            print(f"Skipping move {ending_pos[0]} as it was already executed.")
+        if ending_position[0] in executed_moves:
+            print(f"Skipping move {ending_position[0]} as it was already executed.")
             print(f"Moves executed so far: {executed_moves}")
-            continue  # Salta la mossa che è già stata eseguita
+            continue  # Salta la mossa già eseguita
 
-        choreography = (starting_pos[0],)  # Coreografia iniziale
-        remaining_time = 110.0 / number_of_steps - mandatory_time
-
-        cur_state = (
-            ('choreography', choreography),
-            ('remaining_time', remaining_time),
+        # Creazione stato iniziale e obiettivo per la pianificazione
+        initial_state = (
+            ('choreography', (starting_position[0],)),
+            ('remaining_time', 110.0 / number_of_steps - average_mandatory_time),
             ('moves_done', 0)
         )
-
-        cur_goal_state = (
+        goal_state = (
             ('remaining_time', 0),
             ('moves_done', 2)
         )
 
+        # Mescolare le mosse disponibili per aumentare la varietà
         keys = list(PossibleMoves.items())
         random.shuffle(keys)
         PossibleMoves = dict(keys)
 
-        cur_problem = NaoProblem(cur_state, cur_goal_state, PossibleMoves, 1, solution)
-        cur_solution = iterative_deepening_search(cur_problem)
+        # Creazione del problema e ricerca della soluzione
+        nao_problem = NaoProblem(initial_state, goal_state, PossibleMoves, 1, choreography_solution)
+        nao_solution = iterative_deepening_search(nao_problem)
 
         try:
-            if cur_solution is None:
+            if nao_solution is None:
                 raise RuntimeError(f'Step {index} - no solution was found!')
-            cur_solution_dict = from_state_to_dict(cur_solution.state)
-            cur_choreography = cur_solution_dict['choreography']
-            print(f"Step {index}: \t" + ", ".join(cur_choreography))
-            solution += cur_choreography
-            # Aggiungi la mossa corrente alla lista delle mosse eseguite
-            executed_moves.extend(cur_choreography)
+            solution_state_dict = from_state_to_dict(nao_solution.state)
+            current_choreography = solution_state_dict['choreography']
+            print(f"Step {index}: \t" + ", ".join(current_choreography))
+            choreography_solution += current_choreography
+            executed_moves.extend(current_choreography)
             PossibleMoves = {move: details for move, details in PossibleMoves.items() if move not in executed_moves}
         except:
-            print(f'Step {index}: solving just using the intermediate compulsory positions')
-            for dance in pos_list[abs(index-2):-1]:
-                solution+=(dance[0],)
+            print(f'Step {index}: solving using intermediate compulsory positions')
+            for dance in choreography_positions[abs(index - 2):-1]:
+                choreography_solution += (dance[0],)
             break
             
-
     # Fase di conclusione della pianificazione
-    end_planning = time.time()
-    solution += (final_position[0],)
+    planning_end_time = time.time()
+    choreography_solution += (final_position[0],)
     try:
-        state_dict = from_state_to_dict(cur_solution.state)
+        state_dict = from_state_to_dict(nao_solution.state)
         print("\nINFO:")
-        print(f"Time required for the planning : %.2f seconds." % (end_planning - start_planning))
+        print(f"Time required for the planning : %.2f seconds." % (planning_end_time - planning_start_time))
         print(f"Estimated choreography duration time: {110 - state_dict['remaining_time']}")
-        print("-"*20)
+        print("-" * 20)
     except AttributeError:
-        times=sum(pos[1].Moveduration for pos in pos_list)
-        i=0
-        while i<len(solution):
-            try:
-                times+=PossibleMoves[dance].Moveduration
-                i+=1
-            except KeyError:
-                i+=1
-
-                
-
+        total_duration = sum(pos[1].Moveduration for pos in choreography_positions)
         print("\nINFO:")
-        print(f"Time required for the planning : %.2f seconds." % (end_planning - start_planning))
-        print(f"Estimated choreography duration time: {times}")
-        print("-"*20)
+        print(f"Time required for the planning : %.2f seconds." % (planning_end_time - planning_start_time))
+        print(f"Estimated choreography duration time: {total_duration}")
+        print("-" * 20)
 
-    # Esecuzione della danza
+    # Esecuzione della coreografia
     print("\nRunning...")
     play_song("Daft Punk - Something About Us.mp3")
-    start_dance = time.time()
-    do_moves(solution, robot_ip, port)
-    end_dance = time.time()
-    print(f"Real choreography duration time: %.2f seconds." % (end_dance - start_dance))
+    dance_start_time = time.time()
+    do_moves(choreography_solution, robot_ip, port)
+    dance_end_time = time.time()
+    print(f"Real choreography duration time: %.2f seconds." % (dance_end_time - dance_start_time))
 
 if __name__ == "__main__":
     robot_ip = "127.0.0.1"
--- nao_problem.py
+++ nao_problem.py
@@ -1,53 +1,67 @@
 from aima.search import Problem
 from utils import from_state_to_dict
+
 class NaoProblem(Problem):
 
-    def __init__(self, initial, goal, moves, threshold, previous_moves_done):
-        super().__init__(initial, goal)
-        self.available_moves = moves
-        self.previous_moves_done = previous_moves_done
-        self.time_threshold = threshold
+    def __init__(self, initial_state, goal_state, available_moves, time_buffer, executed_moves):
+        super().__init__(initial_state, goal_state)
+        self.available_moves = available_moves  # Dizionario delle mosse disponibili
+        self.executed_moves = executed_moves  # Lista delle mosse già eseguite
+        self.time_buffer = time_buffer  # Margine di tempo accettabile per il goal
 
-    def is_move_applicable(self, state, move_name, move):
-        state_dict = from_state_to_dict(state)
+    def is_move_valid(self, current_state, move_name, move_details):
+        """
+        Determina se una mossa è valida.
+        Controlla che il tempo rimanente sia sufficiente e che la mossa non sia ripetuta di recente.
+        """
+        state_dict = from_state_to_dict(current_state)
 
-        # Controlla se il tempo rimanente è sufficiente
-        if state_dict['remaining_time'] < move.Moveduration:
+        # Verifica se il tempo rimanente è sufficiente per eseguire la mossa
+        if state_dict['remaining_time'] < move_details.Moveduration:
             return False
 
-        # Controlla che il movimento non sia identico all'ultimo eseguito
-        last_move = state_dict['choreography'][-4:]
-        if move_name in self.previous_moves_done or move_name in last_move:
+        # Verifica che la mossa non sia tra quelle già eseguite o tra le ultime eseguite
+        recent_moves = state_dict['choreography'][-4:]  # Ultime 4 mosse eseguite
+        if move_name in self.executed_moves or move_name in recent_moves:
             return False
-
-        
 
         return True
 
-    def actions(self, state):
-        usable_actions = []
-        for move_name, move in self.available_moves.items():
-            if self.is_move_applicable(state, move_name, move):
-                usable_actions.append(move_name)
-        return usable_actions
+    def actions(self, current_state):
+        """
+        Ritorna una lista di mosse valide che possono essere eseguite nello stato corrente.
+        """
+        valid_actions = []
+        for move_name, move_details in self.available_moves.items():
+            if self.is_move_valid(current_state, move_name, move_details):
+                valid_actions.append(move_name)
+        return valid_actions
 
-    def result(self, state, action):
-        nao_move = self.available_moves[action]
-        state_dict = from_state_to_dict(state)
+    def result(self, current_state, selected_action):
+        """
+        Applica una mossa allo stato corrente e restituisce il nuovo stato.
+        """
+        selected_move = self.available_moves[selected_action]
+        state_dict = from_state_to_dict(current_state)
 
-        return (('choreography', (*state_dict['choreography'], action)),
-                ('remaining_time', state_dict['remaining_time'] - nao_move.Moveduration),
-                ('moves_done', state_dict['moves_done'] + 1))
+        return (
+            ('choreography', (*state_dict['choreography'], selected_action)),  # Aggiunge la mossa alla coreografia
+            ('remaining_time', state_dict['remaining_time'] - selected_move.Moveduration),  # Aggiorna il tempo rimanente
+            ('moves_done', state_dict['moves_done'] + 1)  # Incrementa il conteggio delle mosse
+        )
 
-    def goal_test(self, state):
-        state_dict = from_state_to_dict(state)
+    def goal_test(self, current_state):
+        """
+        Verifica se lo stato corrente soddisfa i criteri del goal.
+        """
+        state_dict = from_state_to_dict(current_state)
         goal_dict = from_state_to_dict(self.goal)
 
+        # Vincolo sul tempo: il tempo rimanente deve essere compreso nel margine accettabile
         goal_remaining_time = goal_dict['remaining_time']
-        a = goal_remaining_time
-        b = goal_remaining_time + self.time_threshold
+        time_constraint = (goal_remaining_time <= state_dict['remaining_time'] <= goal_remaining_time + self.time_buffer)
 
-        # Verifica i vincoli del goal
-        time_constraint = (a <= state_dict['remaining_time'] <= b)
+        # Vincolo sul numero di mosse eseguite
         moves_done_constraint = (state_dict['moves_done'] >= goal_dict['moves_done'])
+
         return time_constraint and moves_done_constraint
